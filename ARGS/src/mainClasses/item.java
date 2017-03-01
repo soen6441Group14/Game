27f4e2ff0a2983c7998ee65551a4bea32851c193
grey <GreyLee369@gmail.com>
2017-03-01 15:42:13 -0500

    "add new constructor"

--- ARGS/src/mainClasses/item.java
+++ ARGS/src/mainClasses/item.java
@@ -16,13 +16,20 @@ public class item implements Serializable {
     itemTypes type;
 
     public String itemCode;
+    public int itemvalue;
 
     public item() {
         itemCode = "-1";
     }
-
+    
     public item(String new_temCode) {
         itemCode = new_temCode;
+    }
+    
+    // i add a new constructor here, since it needs the value of item
+    public item(String new_temCode,int value) {
+        itemCode = new_temCode;
+        itemvalue = value;
     }
 
     public String printItem() {
