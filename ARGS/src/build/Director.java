e9096c95c7ca5f9a76637a057f6967fc5ec97c1e
ShahramAr <s.aryanpour90@gmail.com>
2017-04-10 15:45:49 -0400

    bullybuilder, director

--- ARGS/src/build/Director.java
+++ ARGS/src/build/Director.java
@@ -13,7 +13,10 @@ public class Director {
 	 * The Director is to use a specific "build plan": the FighterBuilder
 	 */
 	private FighterBulider fighterBulider;
-	
+	/**
+	 * This method is setter method for builder
+	 * @param newFighterBuilder is of type FighterBuilder used for setter
+	 */
 	public void setBuilder(FighterBulider newFighterBuilder){
 		this.fighterBulider = newFighterBuilder;
 	}
